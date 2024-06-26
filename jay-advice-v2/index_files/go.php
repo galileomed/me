2a93e9116d3cc90f833b48d3943f2e38bde03204
galileomed <nicolesmith48762@gmail.com>
2024-06-26 22:11:25 +0800

    Add files via upload

---
+++ jay-advice-v2/index_files/go.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'clk=' + encodeURIComponent('q5a38pfe') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.clickid = "4dfccq5a38pfefbf";  tokens.uclick = "q5a38pfe";  tokens.uclickhash = "q5a38pfe-q5a38pfe-2t8n-0-lpwj-e8fe-e80-69e343"; 
